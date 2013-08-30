572c87e383a5ca7bccbd9103d9455d17f9cbc99b
Tobias Roeser <tobias.roeser@tototec.de>
2013-08-30 14:57:05 +0200

    Added buildfile.

---
+++ de.tototec.osgi.setupbuilder/SBuild.scala
@@ -0,0 +1,66 @@
+import de.tototec.sbuild._
+import de.tototec.sbuild.TargetRefs._
+import de.tototec.sbuild.ant._
+import de.tototec.sbuild.ant.tasks._
+
+@version("0.4.0")
+@classpath("http://repo1.maven.org/maven2/org/apache/ant/ant/1.8.4/ant-1.8.4.jar")
+class SBuild(implicit project: Project) {
+
+  val namespace = "de.tototec.osgi.setupbuilder"
+  val version = "0.0.2.9000"
+
+  val scalaVersion = "2.10.2"
+
+  val compileCp =
+    s"mvn:org.scala-lang:scala-library:${scalaVersion}"
+
+  ExportDependencies("eclipse.classpath", compileCp)
+
+  val compilerCp =
+    s"mvn:org.scala-lang:scala-library:${scalaVersion}" ~
+      s"mvn:org.scala-lang:scala-compiler:${scalaVersion}" ~
+      s"mvn:org.scala-lang:scala-reflect:${scalaVersion}"
+
+  val bndCp = "mvn:biz.aQute:bndlib:1.50.0"
+
+  val jar = s"target/${namespace}-${version}.jar"
+
+  Target("phony:all") dependsOn "clean" ~ jar
+
+  Target("phony:clean").evictCache exec {
+    AntDelete(dir = Path("target"))
+  }
+
+  Target("phony:compile").cacheable dependsOn compileCp ~ compilerCp ~ "scan:src/main/scala" exec { ctx: TargetContext =>
+    addons.scala.Scalac(
+      compilerClasspath = compilerCp.files,
+      classpath = compileCp.files,
+      sources = "scan:src/main/scala".files,
+      destDir = Path("target/classes"),
+      deprecation = true,
+      encoding = "UTF-8",
+      target = "jvm-1.5",
+      debugInfo = "vars"
+    )
+  }
+
+  Target(jar) dependsOn "compile" ~ compileCp ~ bndCp exec { ctx: TargetContext =>
+    addons.bnd.BndJar(
+      destFile = ctx.targetFile.get,
+      bndClasspath = bndCp.files,
+      classpath = compileCp.files ++ Seq(Path("target/classes")),
+      props = Map(
+        "Bundle-SymbolicName" -> namespace,
+        "Bundle-Version" -> version,
+        "Implementation-Version" -> version,
+        "Export-Package" -> s"""${namespace};version="${version}"""",
+        "Import-Package" -> s"""scala.*;version="[${scalaVersion},2.11),"
+                                *""",
+        "Include-Resource" -> "../LICENSE.txt",
+        "Scala-Version" -> scalaVersion
+      )
+    )
+  }
+
+}
