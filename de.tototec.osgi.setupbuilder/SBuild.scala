d020b16748143e2505b6479afbae640eb909be81
Tobias Roeser <tobias.roeser@tototec.de>
2013-09-27 13:54:37 +0200

    Added setup for source jar, javadoc jar and mvn staging.

--- de.tototec.osgi.setupbuilder/SBuild.scala
+++ de.tototec.osgi.setupbuilder/SBuild.scala
@@ -25,6 +25,8 @@ class SBuild(implicit project: Project) {
   val bndCp = "mvn:biz.aQute:bndlib:1.50.0"
 
   val jar = s"target/${namespace}-${version}.jar"
+  val sourcesJar = s"target/${namespace}-${version}-sources.jar"
+  val javadocJar = s"target/${namespace}-${version}-javadoc.jar"
 
   Target("phony:all") dependsOn jar
 
@@ -43,6 +45,16 @@ class SBuild(implicit project: Project) {
     )
   }
 
+  Target("phony:scaladoc").cacheable dependsOn compileCp ~ compilerCp ~ "scan:src/main/scala" exec { ctx: TargetContext =>
+    addons.scala.Scaladoc(
+      scaladocClasspath = compilerCp.files,
+      classpath = compileCp.files,
+      sources = "scan:src/main/scala".files,
+      destDir = Path("target/scaladoc"),
+      deprecation = true
+    )
+  }
+
   Target(jar) dependsOn "scan:target/classes" ~ compileCp ~ bndCp ~ "compile" exec { ctx: TargetContext =>
     addons.bnd.BndJar(
       destFile = ctx.targetFile.get,
@@ -55,10 +67,84 @@ class SBuild(implicit project: Project) {
         "Export-Package" -> s"""${namespace};version="${version}"""",
         "Import-Package" -> s"""scala.*;version="[${scalaVersion},2.11),"
                                 *""",
-        "Include-Resource" -> "../LICENSE.txt",
+        "Include-Resource" -> "LICENSE.txt",
         "Scala-Version" -> scalaVersion
       )
     )
+  }
+
+  Target(sourcesJar) dependsOn "scan:src/main/scala" exec { ctx: TargetContext =>
+    AntJar(destFile = ctx.targetFile.get, fileSet = AntFileSet(dir = Path("src/main/scala")))
+  }
+
+  Target(javadocJar) dependsOn "scaladoc" ~~ "scan:target/scaladoc" exec { ctx: TargetContext =>
+    AntJar(destFile = ctx.targetFile.get, fileSet = AntFileSet(dir = Path("target/scaladoc")))
+  }
+
+  Target("phony:prepareMvnStaging") dependsOn sourcesJar ~ jar ~ javadocJar exec {
+    AntMkdir(dir = Path("target/mvn"))
+
+    val pom = s"""<project>
+  <modelVersion>4.0.0</modelVersion>
+  <groupId>de.tototec</groupId>
+  <artifactId>de.tototec.osgi.setupbuilder</artifactId>
+  <packaging>jar</packaging>
+  <version>${version}</version>
+  <name>OSGi Setup Builder</name>
+  <description>Build OSGi Framework setups</description>
+  <url>https://github.com/ToToTec/osgi-setup-builder</url>
+  <licenses>
+    <license>
+      <name>The Apache Software License, Version 2.0</name>
+      <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+      <distribution>repo</distribution>
+    </license>
+  </licenses>
+  <scm>
+    <url>https://github.com/ToToTec/osgi-setup-builder.git</url>
+    <connection>https://github.com/ToToTec/osgi-setup-builder.git</connection>
+  </scm>
+  <developers>
+    <developer>
+      <id>TobiasRoeser</id>
+      <name>Tobias Roeser</name>
+      <email>tobias.roeser@tototec.de</email>
+    </developer>
+  </developers>
+</project>"""
+
+    AntEcho(message = pom, file = Path("target/mvn/pom.xml"))
+
+    val script = s"""#!/bin/sh
+
+echo "Please edit settings.xml with propper connection details."
+read
+
+echo "Uploading jar"
+mvn -s ./settings.xml gpg:sign-and-deploy-file -DpomFile=pom.xml -Dfile=${jar.files.head} -Durl=http://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging
+
+echo "Uploading sources"
+mvn -s ./settings.xml gpg:sign-and-deploy-file -DpomFile=pom.xml -Dfile=${sourcesJar.files.head} -Dclassifier=sources -Durl=http://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging
+
+echo "Uploading javadoc"
+mvn -s ./settings.xml gpg:sign-and-deploy-file -DpomFile=pom.xml -Dfile=${javadocJar.files.head} -Dclassifier=javadoc -Durl=http://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging
+
+"""
+    AntEcho(message = script, file = Path("target/mvn/script.sh"))
+
+    val settings = """<settings>
+  <servers>
+    <server>
+      <id>sonatype-nexus-staging</id>
+      <username>your-username</username>
+      <password>your-password</password>
+    </server>
+  </servers>
+</settings>
+"""
+
+    AntEcho(message = settings, file = Path("target/mvn/settings.xml"))
+
   }
 
 }
